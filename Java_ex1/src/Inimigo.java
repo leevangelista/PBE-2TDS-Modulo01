1ba32c1022fb5dd2db2b5e5f906c30233a4bbc38
GabrielCardoso76 <159193822+GabrielCardoso76@users.noreply.github.com>
2025-01-29 16:35:11 -0300

    Criei a classe inimigo

---
+++ Java_ex1/src/Inimigo.java
@@ -0,0 +1,15 @@
+public class Inimigo extends Personagem, Ataque{
+    //Atributos
+    int id;
+    String nome;// (nome do inimigo)
+    int hp; //(vida total do inimigo)
+    int dano; //(dano que o inimigo causa)
+    //Métodos
+
+    public void atacar(Personagem, Ataque){
+
+    } //Reduz a quantidade de vida do personagem com base no dano do ataque(funcão calcularDanoInimigo).
+    public void receber_dano(){
+
+    }// Reduz a vida do inimigo.
+}
