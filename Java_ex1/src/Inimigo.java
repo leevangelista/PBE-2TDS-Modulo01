934f236d88934c48e66e704b5057d6fece616203
GabrielCardoso76 <159193822+GabrielCardoso76@users.noreply.github.com>
2025-01-29 17:17:38 -0300

    Criei o metodo construtor com paramentros e o metodo receber_dano

--- Java_ex1/src/Inimigo.java
+++ Java_ex1/src/Inimigo.java
@@ -1,15 +1,25 @@
-public class Inimigo extends Personagem, Ataque{
+public class Inimigo {
     //Atributos
     int id;
     String nome;// (nome do inimigo)
     int hp; //(vida total do inimigo)
     int dano; //(dano que o inimigo causa)
     //Métodos
+    public Inimigo(int id, String nome, int hp, int dano){
+        this.id = id;
+        this.nome = nome;
+        this.hp = hp;
+        this.dano = dano;
+    }
 
-    public void atacar(Personagem, Ataque){
+    public void atacar(String Personagem,int Ataque){
 
     } //Reduz a quantidade de vida do personagem com base no dano do ataque(funcão calcularDanoInimigo).
-    public void receber_dano(){
-
+    public void receber_dano(int dano){
+        hp -= dano;
+        if (hp < 0) {
+            hp = 0; // Garante que a vida não fique negativa
+        }
+        System.out.println(nome + " recebeu " + dano + " de dano! Vida restante: " + hp);
     }// Reduz a vida do inimigo.
 }
