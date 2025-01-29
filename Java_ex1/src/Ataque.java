8b0984c58e23615aea77ad134fbd0918b9e61d8f
JoaoBiel11 <joao.pavanete.tds24@gmail.com>
2025-01-29 17:15:33 -0300

    Ataque em processo

--- Java_ex1/src/Ataque.java
+++ Java_ex1/src/Ataque.java
@@ -1,12 +1,18 @@
-public class Ataque {
+public class Ataque extends Personagem {
 
-    int id , dano;
+    int id, dano;
     String tipo;
 
-    //definir metodo construtor
-    public Ataque (int id, int dano, String tipo) {
+    // Definir método construtor
+    public Ataque(int id, int dano, String tipo, String nome, int forca, int inteligencia) {
+        super(nome, forca, inteligencia);  // Adicionando 'nome' no super()
         this.id = id;
         this.dano = dano;
         this.tipo = tipo;
     }
+
+    public void calcularDanoPersonagem() {
+        if (tipo)
+    }
+
 }
