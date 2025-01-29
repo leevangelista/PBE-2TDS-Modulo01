cb2260fe5f0151bd719747962b6bd3284c8265cf
JoaoBiel11 <joao.pavanete.tds24@gmail.com>
2025-01-29 16:34:44 -0300

    Criei o método construtor

---
+++ Java_ex1/src/Ataque.java
@@ -0,0 +1,12 @@
+public class Ataque {
+
+    int id , dano;
+    String tipo;
+
+    //definir metodo construtor
+    public Ataque (int id, int dano, String tipo) {
+        this.id = id;
+        this.dano = dano;
+        this.tipo = tipo;
+    }
+}
