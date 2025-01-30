6c1393e28c2e4bd3dcb2dd3736a863aa153d5a75
Gustavo <gu.nunesx@gmail.com>
2025-01-30 15:13:40 -0300

    Ex1

--- Exerc1-Jogo/Exx/src/Personagem.java
+++ Exerc1-Jogo/Exx/src/Personagem.java
@@ -1,2 +1,73 @@
 package PACKAGE_NAME;public class Personagem {
-}
+        int id ;
+        String nome;
+        float hp;
+        int forca;
+        int inteligencia;
+
+        Personagem(int id, String nome, float hp,int forca, int inteligencia){
+            this.hp = hp;
+            this.nome = nome;
+            this.hp = hp;
+            this.forca = forca;
+            this.inteligencia = inteligencia;
+        }
+
+    public void atacar(Inimigo inimigo, Ataque ataque) {
+        int dano = ataque.calcularDanoPersonagem(this);
+        inimigo.receberDano(dano);
+    }
+
+    public void receberDano(int dano) {
+        this.hp -= dano;
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+        System.out.println(this.nome + " recebeu " + dano + " de dano. Vida restante: " + this.hp);
+    }
+
+        public float getHp() {
+            return hp;
+        }
+
+        public void setHp(float hp) {
+            this.hp = hp;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public int getForca() {
+            return forca;
+        }
+
+        public void setForca(int forca) {
+            this.forca = forca;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public int getInteligencia() {
+            return inteligencia;
+        }
+
+        public void setInteligencia(int inteligencia) {
+            this.inteligencia = inteligencia;
+        }
+
+
+    }
+
+
+
