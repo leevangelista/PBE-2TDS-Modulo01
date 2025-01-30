b1d8ddcabafbc98f14c49537b834a0e2657f5745
antonybrito <antony.brito047@hotmail.com>
2025-01-30 13:45:38 -0300

    Refatorei todas as classes

--- Java_ex1/src/Ataque.java
+++ Java_ex1/src/Ataque.java
@@ -1,12 +1,33 @@
 public class Ataque {
+    private int id;
+    private String tipo; // "fisico" ou "magico"
+    private int dano;
 
-    int id , dano;
-    String tipo;
-
-    //definir metodo construtor
-    public Ataque (int id, int dano, String tipo) {
+    public Ataque(int id, String tipo, int dano) {
         this.id = id;
-        this.dano = dano;
         this.tipo = tipo;
+        this.dano = dano;
+    }
+
+    public int calcularDanoPersonagem(Personagem personagem) {
+        if (this.tipo.equals("fisico")) {
+            return this.dano * personagem.getForca();  // Acesso através do getter
+        } else if (this.tipo.equals("magico")) {
+            return this.dano * personagem.getInteligencia();  // Acesso através do getter
+        }
+        return 0;
+    }
+
+    public int calcularDanoInimigo(Inimigo inimigo) {
+        return this.dano + inimigo.getDano();  // Acesso através do getter
+    }
+
+    // Getters
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getDano() {
+        return dano;
     }
 }
--- Java_ex1/src/Inimigo.java
+++ Java_ex1/src/Inimigo.java
@@ -1,15 +1,40 @@
-public class Inimigo extends Personagem, Ataque{
-    //Atributos
-    int id;
-    String nome;// (nome do inimigo)
-    int hp; //(vida total do inimigo)
-    int dano; //(dano que o inimigo causa)
-    //Métodos
+public class Inimigo {
+    private int id;
+    private String nome;
+    private int hp;
+    private int dano;
 
-    public void atacar(Personagem, Ataque){
+    public Inimigo(int id, String nome, int hp, int dano) {
+        this.id = id;
+        this.nome = nome;
+        this.hp = hp;
+        this.dano = dano;
+    }
 
-    } //Reduz a quantidade de vida do personagem com base no dano do ataque(funcão calcularDanoInimigo).
-    public void receber_dano(){
+    public void atacar(Personagem personagem, Ataque ataque) {
+        int dano = ataque.calcularDanoInimigo(this);
+        System.out.println(this.nome + " ataca " + personagem.getNome() + " com ataque " + ataque.getTipo() + ", causando " + dano + " de dano.");
+        personagem.receberDano(dano);
+    }
 
-    }// Reduz a vida do inimigo.
+    public void receberDano(int dano) {
+        this.hp -= dano;
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+        System.out.println(this.nome + " recebeu " + dano + " de dano. HP restante: " + this.hp);
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getDano() {
+        return dano;
+    }
 }
