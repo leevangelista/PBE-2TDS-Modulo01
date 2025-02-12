57495998416f217be41d77dc0e95dbef750a53a5
AntonyBrito <antony.brito047@hotmail.com>
2025-02-12 16:44:09 -0300

    Estrutura

---
+++ Exerc7-Java-Spring-DuasRotas/ex7/src/test/java/com/exercicio/ex7/Ex7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercicio.ex7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
