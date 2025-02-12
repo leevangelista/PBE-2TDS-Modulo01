40f45e1348d2c7eb228a17b693637957dd0e1ca4
JoaoBiel11 <joao.pavanete.tds24@gmail.com>
2025-02-12 16:46:45 -0300

    Base do projeto

---
+++ Exerc7-Java-Spring-DuasRotas/aula07/aula07/src/main/java/com/example/aula07/Aula07Application.java
@@ -0,0 +1,14 @@
+package com.example.aula07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula07Application.class, args);
+		System.out.println("Olá");
+	}
+
+}
