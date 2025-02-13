15f5953e7b6b8a2b002fdfdf2b23d68246443cb8
Davimazoti <davilmazoti07@gmail.com>
2025-02-13 14:42:03 -0300

    Envio The Finals

---
+++ Exerc7-Java-Spring-DuasRotas/exercicio7/src/main/java/com/example/exercicio7/Funcionario.java
@@ -0,0 +1,38 @@
+package com.example.exercicio7;
+
+public class Funcionario {
+
+    private int id;
+    private String nome;
+    private Departamento departamento;
+
+    public Funcionario(int id, String nome, Departamento departamento) {
+        this.id = id;
+        this.nome = nome;
+        this.departamento = departamento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+}
