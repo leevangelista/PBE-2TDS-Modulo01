c26c5e7aca15b55bca1fe6e48e649ac4b58c824f
alexsant22 <alexandre.santana2201@gmail.com>
2025-02-15 01:01:03 -0300

    Created Project

---
+++ Exerc9-MVC-SemBanco-FuncionarioDepartamento/Exerc9_MVC/src/main/java/com/example/Exerc9_MVC/Exerc9MvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.Exerc9_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exerc9MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exerc9MvcApplication.class, args);
+	}
+
+}
---
+++ Exerc9-MVC-SemBanco-FuncionarioDepartamento/Exerc9_MVC/src/test/java/com/example/Exerc9_MVC/Exerc9MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Exerc9_MVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exerc9MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
