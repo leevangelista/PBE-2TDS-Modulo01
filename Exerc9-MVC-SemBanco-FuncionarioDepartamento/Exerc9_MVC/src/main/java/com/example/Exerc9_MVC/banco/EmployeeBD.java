1d864619a41add58d20283cabb16015f630525e1
alexsant22 <alexandre.santana2201@gmail.com>
2025-02-16 03:34:42 -0300

    EmployeeBD - Banco

---
+++ Exerc9-MVC-SemBanco-FuncionarioDepartamento/Exerc9_MVC/src/main/java/com/example/Exerc9_MVC/banco/EmployeeBD.java
@@ -0,0 +1,54 @@
+package com.example.Exerc9_MVC.banco;
+
+import com.example.Exerc9_MVC.model.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeBD {
+
+    public List<Employee> employees;
+
+    public EmployeeBD() {
+        this.employees = new ArrayList<>();
+    }
+
+    // Buscando todos empregados
+    public List<Employee> findAll() {
+        return new ArrayList<>(employees);
+    }
+
+    // Busca apenas um empregado
+    public Employee getByID(Long id) {
+        return employees.stream()
+                .filter(employee -> employee.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    // Inserir empregado
+    public boolean insert(Employee employee) {
+        employees.add(employee);
+        return true;
+    }
+
+    // Update empregado
+    public boolean update(Long id, Employee employee) {
+        Employee employeeBD = employees.stream()
+                .filter(employeeFilter -> employeeFilter.getId() == id)
+                .findFirst()
+                .orElse(null);
+
+        if (employeeBD == null) {
+            return  false;
+        }
+
+        employeeBD.setNome(employee.getNome());
+        employeeBD.setSobrenome(employee.getSobrenome());
+        employeeBD.setCurso(employee.getCurso());
+        employeeBD.setSalario(employee.getSalario());
+        employeeBD.setDepartment(employee.getDepartment());
+
+        return true;
+    }
+}
