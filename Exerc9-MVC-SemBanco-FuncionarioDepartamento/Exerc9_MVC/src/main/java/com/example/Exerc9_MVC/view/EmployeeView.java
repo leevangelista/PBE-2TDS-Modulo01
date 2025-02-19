ead191058dbdb739d72960fadb2ca465cf1863f4
alexsant22 <alexandre.santana2201@gmail.com>
2025-02-19 14:31:28 -0300

    EmployeeView - View

---
+++ Exerc9-MVC-SemBanco-FuncionarioDepartamento/Exerc9_MVC/src/main/java/com/example/Exerc9_MVC/view/EmployeeView.java
@@ -0,0 +1,35 @@
+package com.example.Exerc9_MVC.view;
+
+import com.example.Exerc9_MVC.controller.EmployeeController;
+import com.example.Exerc9_MVC.model.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeView {
+
+    // Instanciando Conroller
+    EmployeeController employeeController = new EmployeeController();
+
+    @GetMapping
+    public List<Employee> getAll() { // Buscando por todos empregados
+        return employeeController.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getById(@PathVariable Long id) { // Buscando por id
+        return employeeController.getById(id);
+    }
+
+    @PostMapping
+    public boolean insert(@RequestBody Employee employee) { // Criando empregado
+        return employeeController.insert(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee update(@RequestBody Employee employee, @PathVariable Long id) { // Update empregado
+        return employeeController.update(id, employee);
+    }
+}
