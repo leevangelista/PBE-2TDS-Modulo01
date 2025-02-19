f913de4d4732e07cb7ca98126a456c0006a26834
gustavo <gu.nunesx@gmail.com>
2025-02-19 14:04:12 -0300

    gustavo

---
+++ Exerc9-MVC-SemBanco-FuncionarioDepartamento/ex9/src/main/java/com/example/ex9/Funcionario.java
@@ -0,0 +1,68 @@
+package com.example.ex9;
+
+public class Funcionario {
+
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String curso;
+    private double salario;
+    private Departamento departamento;
+
+    public Funcionario(Long id, String nome, String sobrenome, String curso, double salario, Departamento departamento) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.curso = curso;
+        this.salario = salario;
+        this.departamento = departamento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+}
