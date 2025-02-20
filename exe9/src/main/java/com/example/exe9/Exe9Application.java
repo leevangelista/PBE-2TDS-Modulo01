3f060ada71249e375c3bd9f7eddad05f19dc162a
Guilherme Andrade Camikado <guilherme.camikado@gmail.com>
2025-02-20 16:38:42 -0300

    Add files via upload
    Exe9

---
+++ exe9/src/main/java/com/example/exe9/Exe9Application.java
@@ -0,0 +1,13 @@
+package com.example.exe9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exe9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exe9Application.class, args);
+	}
+
+}
