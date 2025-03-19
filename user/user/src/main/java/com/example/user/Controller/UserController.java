49b0f01528cb59a87586b0ea61473b12e4f291da
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-19 17:01:41 -0300

    up

---
+++ user/user/src/main/java/com/example/user/Controller/UserController.java
@@ -0,0 +1,72 @@
+package com.example.user.Controller;
+
+
+import com.example.user.Entity.User;
+import com.example.user.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){
+        User userBd = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userBd);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable long id, @RequestBody User user) {
+       Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.FOUND).body(user);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateById(@PathVariable long id, @RequestBody User user){
+       Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isPresent()){
+       user.setNome(user.getNome());
+       user.setEmail(user.getEmail());
+            return ResponseEntity.status(HttpStatus.OK).body(user);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> delete(@PathVariable long id, @RequestBody User user){
+        Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isPresent()){
+            userRepository.delete(user);
+            return ResponseEntity.status(HttpStatus.OK).body(user);
+        }
+        else{
+            return null;
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll(){
+        List<User> userList = userRepository.findAll();
+        if(userList.isEmpty()){
+            return ResponseEntity.status(HttpStatus.FOUND).body(userList);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+}
---
+++ user/user/src/main/java/com/example/user/Entity/User.java
@@ -0,0 +1,50 @@
+package com.example.user.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private long id;
+    private String nome;
+    private String email;
+
+    public User(long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
